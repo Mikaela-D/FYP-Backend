4ef48e9d7b53940373875677567ec81ceaff0279
Mikaela-D <G00401392@atu.ie>
2025-04-19 22:27:03 +0100

    Add password field to agent schema

--- routes/agents.js
+++ routes/agents.js
@@ -16,6 +16,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/db", {
 const agentSchema = new Schema(
   {
     name: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
   },
   { collection: "agents" }
 );
